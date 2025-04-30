f3a26442ca2330bf262d40dae79571112956e1d7
andrew <elkin.andr@gmail.com>
2025-04-30 16:47:13 +0300

    bump version for test

--- mindsdb_sql_parser/__about__.py
+++ mindsdb_sql_parser/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'mindsdb_sql_parser'
 __package_name__ = 'mindsdb_sql_parser'
-__version__ = '0.6.0'
+__version__ = '0.6.1'
 __description__ = "Mindsdb SQL parser"
 __email__ = "jorge@mindsdb.com"
 __author__ = 'MindsDB Inc'
