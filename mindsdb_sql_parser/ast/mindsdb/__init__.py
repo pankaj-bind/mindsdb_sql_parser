8bad192fb444c52d84b6458a6ea690a342b8be16
Fedor Shabashev <fedor.shabashev@gmail.com>
2025-05-16 17:20:10 +0100

    adding imports (#23)

--- mindsdb_sql_parser/ast/mindsdb/__init__.py
+++ mindsdb_sql_parser/ast/mindsdb/__init__.py
@@ -15,7 +15,7 @@ from .create_job import CreateJob
 from .drop_job import DropJob
 from .chatbot import CreateChatBot, UpdateChatBot, DropChatBot
 from .trigger import CreateTrigger, DropTrigger
-from .knowledge_base import CreateKnowledgeBase, DropKnowledgeBase
+from .knowledge_base import CreateKnowledgeBase, DropKnowledgeBase, CreateKnowledgeBaseIndex, DropKnowledgeBaseIndex
 from .skills import CreateSkill, DropSkill, UpdateSkill
 
 # remove it in next release
